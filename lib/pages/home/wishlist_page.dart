3257745acd6b220478b6e9ffb73b73b46de86ee7
Isa Maulana Tantra <isamaulanatantra@gmail.com>
2022-06-22 15:48:33 +0700

    25

--- lib/pages/home/wishlist_page.dart
+++ lib/pages/home/wishlist_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
+import 'package:shamo/widgets/wishlist_card.dart';
 
 class WishlistPage extends StatelessWidget {
   // const WishlistPage({Key? key}) : super(key: key);
@@ -77,12 +78,31 @@ class WishlistPage extends StatelessWidget {
     );
   }
 
+  Widget content() {
+    return Expanded(
+      child: Container(
+        color: backgroundColor3,
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          children: [
+            WishlistCard(),
+            WishlistCard(),
+            WishlistCard(),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         header(),
-        emptyWishlist(),
+        // emptyWishlist(),
+        content(),
       ],
     );
   }
